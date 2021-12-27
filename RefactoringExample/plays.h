81563d1a21c9840555101878eb7ee8040268ceb3
Ryan Flynn <ryanflynn365@gmail.com@gmail.com>
2021-12-27 13:34:59 -0800

    Add a new play type and an instance of it in "plays".  This won't change any test behavior because we haven't added this play to the test invoice yet.

--- RefactoringExample/plays.h
+++ RefactoringExample/plays.h
@@ -3,11 +3,20 @@
 #include <string>
 
 
+/*
+Class to organize Plays. Each play type (comedy, tragedy, etc) has
+its own rules for how to calculate the total cost and the number of
+volume credits, based on the audience size.  volumeCredits has a default
+implementation, but cost MUST be overridden on subclasses.
+
+To introduce a new play type, simply subclass from Play and provide an
+override for amountFor.  Optionally also override volumeCreditsFor.
+*/
+
 class Play {
 public:
 	std::string m_name;
 	Play(std::string name="") : m_name(name) {}
-
 	virtual int amountFor(int audience) = 0;
 	virtual int volumeCreditsFor(int audience) { 
 		return std::max(audience - 30, 0);
@@ -28,7 +37,6 @@ public:
 	}
 	int volumeCreditsFor(int audience) { 
 		int result =  Play::volumeCreditsFor(audience);
-		// Add extra credit for every ten comedy attendees
 		result += std::floor(audience / 5);
 		return result;
 	}
@@ -44,12 +52,31 @@ public:
 	}
 };
 
+class ChildrensPlay : public Play {
+public:
+	ChildrensPlay(std::string name = "") : Play(name) {}
+	int amountFor(int audience) override {
+		int result = 20000;
+		if (audience > 40) { result += 500 * (audience - 40); }
+		return result;
+	}
+	int volumeCreditsFor(int audience) override {
+		int result = Play::volumeCreditsFor(audience);
+		result += std::floor(audience / 2);
+		return result;
+	}
+};
+
+
 Tragedy hamlet("Hamlet");
 Comedy aslike("As you like it");
 Tragedy othello("Othello");
+ChildrensPlay barney("Barney On Ice");
 
 struct Plays {
 	std::unordered_map<std::string, Play*> m_plays;
 	Plays(std::unordered_map<std::string, Play*> plays) : m_plays(plays) {}
 };
-Plays plays({ {"hamlet", &hamlet}, {"as-like", &aslike}, {"othello", &othello} });
+Plays plays({
+	{"hamlet", &hamlet}, {"as-like", &aslike},
+	{"othello", &othello}, {"barney", &barney} });
